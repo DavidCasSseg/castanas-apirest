123f5cc5191e25d1ef831b94ce64f26be36a1c37
DavidCasSseg <109768908+DavidCasSseg@users.noreply.github.com>
2022-07-25 11:52:56 -0500

    commit2

---
+++ src/main/resources/import.sql
@@ -0,0 +1,11 @@
+/* Populate tabla clientes */
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES('Leticia', 'Cuaya', 'leticia.cuaya@mail.com', '2022-07-23');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES('David', 'Castañeda', 'davcas@mail.com', '2022-07-23');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES('Benjamin', 'Castañeda', 'benjin20@mail.com', '2022-07-23');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES('Karen', 'Segura', 'karen.segura@mail.com', '2022-07-23');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES('Andres', 'Guzman', 'andres.guzman@mail.com', '2022-07-23');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES('Mario', 'Bros', 'mario.bros@mail.com', '2022-07-23');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES('Tornado', 'Roe', 'tornado.roe@mail.com', '2022-07-23');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES('Magma', 'Lee', 'magma.lee@mail.com', '2022-07-23');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES('Jade', 'Doe', 'jade.doe@mail.com', '2022-07-23');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES('Emmet', 'Brown', 'emmet.brown@mail.com', '2022-07-23');
